9e66431a1ba92b69897b50d9b265450ebf4ad40e
aturi <angelaturi99@gmail.com>
2021-04-28 11:27:10 -0400

    Create minesweeper

---
+++ minesweeper/components/game.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import * as Minesweeper from "../minesweeper.js";
+import Board from "./board";
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            board: new Minesweeper.Board(10, 10)
+        }
+        this.updateGame = this.updateGame.bind(this)
+    }
+
+    updateGame() {
+        
+    }
+
+    render() {
+        return (
+            <Board board={this.state.board} updateGame={this.updateGame}/>
+        )
+    }
+}
+
+export default Game;
+
